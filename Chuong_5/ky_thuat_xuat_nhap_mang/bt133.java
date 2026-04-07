6fbf075a968466725d61e51b730d114d3d1e2e6d
tienhq <tienhq@ciaosoft.vn>
2026-04-07 16:18:53 +0700

    Update bt133.java

--- Chuong_5/ky_thuat_xuat_nhap_mang/bt133.java
+++ Chuong_5/ky_thuat_xuat_nhap_mang/bt133.java
@@ -1,4 +1,4 @@
-package bt_java_tienhq.Chuong_5;
+package bt_java_tienhq.Chuong_5.ky_thuat_xuat_nhap_mang;
 
 //Viết hàm liệt kê các vị trí mà giá trị tại đó là giá trị âm trong mảng một chiều các số thực.. 
 import java.util.Scanner;
