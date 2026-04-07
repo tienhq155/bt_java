efc79f1605c4dd59e339d522613e3c9689d02344
tienhq <tienhq@ciaosoft.vn>
2026-04-07 15:48:45 +0700

    thiếu "nếu không có số chẵn về trả về -1"

--- Chuong_5/bt136.java
+++ Chuong_5/bt136.java
@@ -1,7 +1,8 @@
 package bt_java_tienhq.Chuong_5;
 
 import java.util.Scanner;
-
+//Tìm “số chẵn cuối cùng” trong mảng một chiều các số nguyên 
+//(chancuoi). Nếu mảng không có giá trị chẵn thì trả về giá trị -1.
 public class bt136 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,8 +21,13 @@ public class bt136 {
             if (arr[i] % 2 == 0) {
                 System.out.print("\nSố chẵn cuối cùng của mảng là " + arr[i]);
                 break;
-            }
+            }    
+            // else{
+            //     System.out.print("\nSố chẵn cuối cùng của mảng là " + (-1));
+            // }
         }
+        
         sc.close();
     }
+    
 }
