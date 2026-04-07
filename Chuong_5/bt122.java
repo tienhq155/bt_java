b1b43fbda84c8db703f8fa5d5b84f3b28f5ffe39
tienhq <tienhq@ciaosoft.vn>
2026-04-07 14:28:27 +0700

    Create bt122.java

---
+++ Chuong_5/bt122.java
@@ -0,0 +1,23 @@
+package Chuong_5;
+
+import java.util.Scanner;
+
+public class bt122 {
+        public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử : ");
+
+        long[] arr = new long[sc.nextInt()];
+        for (int i = 0 ; i < arr.length; i++){
+            System.out.print("Nhập Phần Tử Thứ " + (i+1) + ":");
+            arr[i] = sc.nextLong();
+        }
+        double max = arr[0];
+        for (double x : arr){
+            if(x > max){
+                max = x;
+            }
+        }
+        System.out.println("Giá trị lớn nhất trong mảng là : " + max);
+    }
+}
