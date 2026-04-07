345ba8fd6936c0bbd727e162eae01240c4b956dc
tienhq <tienhq@ciaosoft.vn>
2026-04-07 16:13:47 +0700

    Create bt129_131.java

---
+++ Chuong_5/ky_thuat_xuat_nhap_mang/bt129_131.java
@@ -0,0 +1,23 @@
+package bt_java_tienhq.Chuong_5;
+
+// Viết hàm nhập,xuất mảng một chiều các số nguyên.
+import java.util.Scanner;
+
+public class bt129_131 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nNhập số lượng phần tử : ");
+
+        long[] arr = new long[sc.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Nhập Phần Tử Thứ " + (i + 1) + ":");
+            arr[i] = sc.nextLong();
+        }
+        System.out.print("Mảnh Vừa NHập Là : ");
+        for (long x : arr) {
+            System.out.print(x + " ");
+        }
+        sc.close();
+    }
+
+}
