d0364884a445275699110d63b147a64ba0a342a6
tienhq <tienhq@ciaosoft.vn>
2026-04-07 15:21:28 +0700

    Update bt123.java

--- Chuong_5/bt123.java
+++ Chuong_5/bt123.java
@@ -1,5 +1,7 @@
 package bt_java_tienhq.Chuong_5;
+
 import java.util.Scanner;
+
 // Viết hàm tìm một vị trí mà giá trị tại vị trí đó là một giá trị nhỏ nhất trong mảng một chiều các số nguyên. 
 public class bt123 {
     public static void main(String[] args) {
@@ -7,15 +9,15 @@ public class bt123 {
         System.out.print("Nhập số lượng phần tử : ");
         long[] arr = new long[sc.nextInt()];
         int i = 0;
-        int min  = 0;
-        do{
+        int min = 0;
+        do {
             System.out.print("Nhập Phần Tử Thứ " + (i + 1) + ":");
             arr[i] = sc.nextLong();
-            if (arr[i] < arr[min]){
+            if (arr[i] < arr[min]) {
                 min = i;
             }
             i++;
-        }while(i < arr.length);
-        System.out.println("Vị trí của giá trị nhỏ nhất là : " + (min+1));
+        } while (i < arr.length);
+        System.out.println("Vị trí của giá trị nhỏ nhất là : " + (min + 1));
     }
 }
