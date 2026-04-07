497cf9a0ba226c5d33d40bd01cb99225a3ccb101
tienhq <tienhq@ciaosoft.vn>
2026-04-07 15:18:55 +0700

    Create bt128_130.java

---
+++ Chuong_5/bt128_130.java
@@ -0,0 +1,19 @@
+package bt_java_tienhq.Chuong_5;
+// Viết hàm nhập,xuất mảng một chiều các số thực.
+import java.util.Scanner;
+public class bt128_130 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nNhập số lượng phần tử : ");
+
+        double[] arr = new double[sc.nextInt()];
+        for (int i = 0 ; i < arr.length; i++){
+            System.out.print("Nhập Phần Tử Thứ " + (i+1) + ":");
+            arr[i] = sc.nextDouble();
+        }
+        System.out.print("Mảnh Vừa NHập Là : ");
+        for (double x : arr){System.out.print(x + " ");}
+        sc.close();
+    }
+    
+}
