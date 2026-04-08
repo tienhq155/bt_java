5714a5e90083118ed93949d7cb250440dd452259
tienhq <tienhq@ciaosoft.vn>
2026-04-08 16:23:42 +0700

    Create bt142.java

---
+++ Chuong_5/bt_luyen_tap/bt142.java
@@ -0,0 +1,31 @@
+package bt_java_tienhq.Chuong_5.bt_luyen_tap;
+
+import java.util.Scanner;
+
+//Tìm “giá trị nhỏ nhất” trong mảng một chiều số thực (nhonhat).
+public class bt142 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nNhập số lượng phần tử : ");
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Nhập Phần Tử Thứ " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Mảnh Vừa NHập Là : ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        int nhonhat = 0;
+        for (int x : arr) {
+            if (nhonhat == -1 || x < nhonhat) {
+                nhonhat = x;
+            }
+        }
+        System.out.print("\n Giá trị nhỏ nhất trong mảng là " + nhonhat);
+        sc.close();
+    }
+
+}
