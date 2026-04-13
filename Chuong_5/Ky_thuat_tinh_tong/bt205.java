d53281ddcf9271c90db06119c18680fd4728e4a4
tienhq <tienhq@ciaosoft.vn>
2026-04-13 16:37:22 +0700

    Create bt205.java

---
+++ Chuong_5/Ky_thuat_tinh_tong/bt205.java
@@ -0,0 +1,22 @@
+package bt_java_tienhq.Chuong_5.Ky_thuat_tinh_tong;
+
+import java.util.Scanner;
+
+// Tính tổng các giá trị lớn hơn trị tuyệt đối của giá trị đứng liền sau nó
+// trong mảng một chiều các số thực
+public class bt205 {
+    public static void main(String[] arger) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nNhập số lượng phần tử : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Nhập Phần Tử Thứ " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Mảnh Vừa NHập Là : ");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
